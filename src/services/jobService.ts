912d611aa64d8365375d8aee728317f323998bea
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-09-27 09:21:35 +0200

    ✨ change to the right interfaces

--- src/services/jobService.ts
+++ src/services/jobService.ts
@@ -1,24 +1,12 @@
+import { IJob } from '../models/IJob.ts';
+import { IJobsSearchResponse } from '../models/IJobsSearchResponse.ts';
 import { get } from './serviceBase.ts';
 
 // TODO: CHANGE INTO RIGHT BASE_URL
 const BASE_URL = 'https://jobsearch.api.jobtechdev.se/search?offset=0&limit=10';
 
-// TODO: CHANGE TO CORRECT INTERFACE HERE
-export interface IJob {
-  id: number;
-  headline: string;
-  description: {
-    text: string;
-  };
-}
-
-// TODO: CHANGE TO CORRECT INTERFACE HERE
-interface IJobSearchResponse {
-  hits: IJob[];
-}
-
 export const getJobs = async (): Promise<IJob[]> => {
-  const response = await get<IJobSearchResponse>(BASE_URL);
+  const response = await get<IJobsSearchResponse>(BASE_URL);
 
   return response.data.hits;
 };
