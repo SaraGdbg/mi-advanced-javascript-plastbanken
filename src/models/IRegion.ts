a6e5fe7ef37c7df4d6bb625288c9f1e13e1016f8
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-09-27 14:14:33 +0200

    :tada: created functions to get regions and municipalities, and to add them into one combined array with objects

--- src/models/IRegion.ts
+++ src/models/IRegion.ts
@@ -1,6 +1,9 @@
+import { IMunicipality } from './IMunicipality';
+
 export interface IRegion {
   'taxonomy/national-nuts-level-3-code-2019': string;
   'taxonomy/id': string;
   'taxonomy/type': string;
   'taxonomy/preferred-label': string;
+  municipalities: IMunicipality[];
 }
---
+++ src/services/locationsService.ts
@@ -0,0 +1,49 @@
+import { IMunicipality } from '../models/IMunicipality';
+import { IRegion } from '../models/IRegion';
+import { get } from './serviceBase';
+
+const TAXONOMY_URL =
+  'https://taxonomy.api.jobtechdev.se/v1/taxonomy/specific/concepts/';
+
+const getMunicipalities = async () => {
+  const response = await get<IMunicipality[]>(TAXONOMY_URL + 'municipality');
+  return response.data;
+};
+
+const getRegions = async () => {
+  const response = await get<IRegion[]>(TAXONOMY_URL + 'region');
+  return response.data.filter(
+    (word) => word['taxonomy/national-nuts-level-3-code-2019'] !== undefined,
+  );
+};
+
+const mergeRegionsWithMunicipalities = (
+  regions: IRegion[],
+  municipalities: IMunicipality[],
+) => {
+  for (let i = 0; i < regions.length; i++) {
+    const regionID = regions[i]['taxonomy/national-nuts-level-3-code-2019'];
+    for (let j = 0; j < municipalities.length; j++) {
+      const muniID =
+        municipalities[j]['taxonomy/lau-2-code-2015'][0] +
+        municipalities[j]['taxonomy/lau-2-code-2015'][1];
+      if (muniID === regionID) {
+        if (regions[i].municipalities === undefined) {
+          regions[i].municipalities = [municipalities[j]];
+        } else {
+          regions[i].municipalities = [
+            ...regions[i].municipalities,
+            municipalities[j],
+          ];
+        }
+      }
+    }
+  }
+  return regions;
+};
+
+export const getLocations = async () => {
+  const regions = await getRegions();
+  const municipalities = await getMunicipalities();
+  return mergeRegionsWithMunicipalities(regions, municipalities);
+};
