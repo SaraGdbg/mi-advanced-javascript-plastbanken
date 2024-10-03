353f2ee40f6864adce7693f20686b243779dd22f
Sara Gdbg <146811766+SaraGdbg@users.noreply.github.com>
2024-10-03 19:31:40 +0200

    🚧 Initiate functions to handle change in sorting function

--- src/components/SortResults.tsx
+++ src/components/SortResults.tsx
@@ -1,19 +1,43 @@
-import { DigiFormFilter } from '@digi/arbetsformedlingen-react';
+import { DigiContextMenu } from '@digi/arbetsformedlingen-react';
 
 export const SortResults = () => {
   //TODO: logic to only be able to check one of the checkboxes at a time
   // TODO: dispatches for each choice
+
+  interface IMenuItems {
+    id: number;
+    title: string;
+  }
+
+  const menuItems: IMenuItems[] = [
+    {
+      id: 0,
+      title: 'Relevans',
+    },
+    {
+      id: 1,
+      title: 'Datum fallande',
+    },
+    {
+      id: 2,
+      title: 'Datum stigande',
+    },
+  ];
+
+  const handleChange = (e) => {
+    console.log('handleChange');
+  };
+
   return (
     <>
-      <DigiFormFilter
-        afFilterButtonText="Sortera efter"
-        afSubmitButtonText="Filtrera"
-        afListItems={[
-          { id: 'omr1', label: 'Relevans' },
-          { id: 'omr2', label: 'Datum stigande' },
-          { id: 'omr3', label: 'Datum fallande' },
-        ]}
-      ></DigiFormFilter>
+      <DigiContextMenu
+        afTitle="Sortera efter"
+        afMenuItems={menuItems}
+        onAfChangeItem={(e) => {
+          console.log('Valt alternativ', e.detail.item, e.detail.idx);
+          handleChange(e);
+        }}
+      ></DigiContextMenu>
     </>
   );
 };
