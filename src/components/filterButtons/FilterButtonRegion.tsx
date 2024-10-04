90f701858265d46cbb3cef4f282a02d8d77f7aa3
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-10-04 10:34:25 +0200

    added close on click outside of menu

--- src/components/filterButtons/FilterButtonRegion.tsx
+++ src/components/filterButtons/FilterButtonRegion.tsx
@@ -12,41 +12,32 @@ export const FilterButtonRegion = () => {
   const [viewMenu, setViewMenu] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
-  // useEffect(() => {
-  //   const close = (e: MouseEvent) => {
-  //     if (
-  //       dropdownRef.current &&
-  //       !dropdownRef.current.contains(e.target as Node)
-  //     ) {
-  //       setViewMenu(false);
-  //     }
-  //   };
-  //   document.addEventListener('click', close, true);
-
-  //   return () => {
-  //     document.removeEventListener('click', close, true);
-  //   };
-  // });
-
   const toggleMenu = () => {
     setViewMenu(!viewMenu);
   };
 
   return (
     <>
-      <div className="menuContainer">
-        <div className="filterButtonContainer" ref={dropdownRef}>
-          <DigiButton
-            afSize={ButtonSize.MEDIUM}
-            afVariation={ButtonVariation.SECONDARY}
-            afFullWidth={false}
-            onClick={toggleMenu}
-          >
-            Ort
-            <DigiIconChevronDown slot="icon-secondary" />
-          </DigiButton>
+      <div>
+        <div className="menuContainer">
+          <div className="filterButtonContainer" ref={dropdownRef}>
+            <DigiButton
+              afSize={ButtonSize.MEDIUM}
+              afVariation={ButtonVariation.SECONDARY}
+              afFullWidth={false}
+              onClick={toggleMenu}
+            >
+              Ort
+              <DigiIconChevronDown slot="icon-secondary" />
+            </DigiButton>
+          </div>
+          {viewMenu === true ? <RegionMenu></RegionMenu> : ''}
         </div>
-        {viewMenu === true ? <RegionMenu></RegionMenu> : ''}
+        {viewMenu === true ? (
+          <div className="overlay" onClick={toggleMenu}></div>
+        ) : (
+          ''
+        )}
       </div>
     </>
   );
