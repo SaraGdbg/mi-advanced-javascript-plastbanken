a78ea574044ac09641d678e901594a9bf73a8680
Sara Gdbg <146811766+SaraGdbg@users.noreply.github.com>
2024-10-04 12:16:49 +0200

    🚧 Add switch

--- src/components/SortResults.tsx
+++ src/components/SortResults.tsx
@@ -1,4 +1,8 @@
 import { DigiContextMenu } from '@digi/arbetsformedlingen-react';
+import {
+  ContextMenuEventItem,
+  DigiContextMenuCustomEvent,
+} from '@digi/arbetsformedlingen/dist/types/components';
 
 export const SortResults = () => {
   //TODO: logic to only be able to check one of the checkboxes at a time
@@ -24,19 +28,41 @@ export const SortResults = () => {
     },
   ];
   //Add type to event
-  const handleChange = (e) => {
-    console.log('handleChange');
+  const handleChange = (
+    e: DigiContextMenuCustomEvent<ContextMenuEventItem>,
+  ) => {
+    if (e.detail.idx === 2) {
+      console.log('det funkar');
+      console.log(e.detail.item.id);
+    }
+    let selectedSorting: string = '';
+    switch (e.detail.item.id) {
+      case 0:
+        selectedSorting = 'relevance';
+        break;
+      case 1:
+        selectedSorting = 'pubdate-desc';
+        break;
+      case 2:
+        selectedSorting = 'pubdate-asc';
+        break;
+    }
+    console.log(selectedSorting);
   };
+
+  // vill skicka in en string: relevance, pubdate-desc, pubdate-asc som payload
+  // vill göra en dispatch med valt värde
+  // skapa en funktion som sätter en varibel typ selectedsorting
+  // skicka med den variabeln i dispatchen  till filterreducern: FilterActionType.SET_SORT_BY;
+  // får tillbaka filtrerat resultat
+  // visa på sidan via navigate
 
   return (
     <>
       <DigiContextMenu
         afTitle="Sortera efter"
         afMenuItems={menuItems}
-        onAfChangeItem={(e) => {
-          console.log('Valt alternativ', e.detail.item, e.detail.idx);
-          handleChange(e);
-        }}
+        onAfChangeItem={handleChange}
       ></DigiContextMenu>
     </>
   );
