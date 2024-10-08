00acc2d935ca3f25ddfa9c1a1426e4eccb65e438
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-10-08 12:53:24 +0200

    💄 add icons and logo to UI

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,23 +2,36 @@ import {
   DigiHeader,
   DigiNavigationSidebarButton,
 } from '@digi/arbetsformedlingen-react';
+import styled from 'styled-components';
+
+const StyledDigiHeader = styled(DigiHeader)`
+  svg {
+    display: none;
+  }
+
+  .digi-logo__img {
+    background-image: url('/img/logo_dark_green.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    width: 44px;
+    height: 44px;
+  }
+`;
 
 export const Nav = () => {
   return (
     <>
-      <DigiHeader
+      <StyledDigiHeader
         afSystemName="Plastbanken"
         afHideSystemName={false}
         afMenuButtonText="Meny"
       >
         <a slot="header-logo" aria-label="Startsida" href="/"></a>
         <div slot="header-content">
-          {' '}
-          <DigiNavigationSidebarButton afText="Meny">
-          </DigiNavigationSidebarButton>
+          <DigiNavigationSidebarButton afText="Meny"></DigiNavigationSidebarButton>
         </div>
         <div slot="header-navigation"></div>
-      </DigiHeader>
+      </StyledDigiHeader>
     </>
   );
 };
