6587ec4ab61380f99e1f1e2e2d4c585c7ae25947
Sara Gdbg <146811766+SaraGdbg@users.noreply.github.com>
2024-10-09 17:28:58 +0200

    💄 Add StyledSpan

--- src/components/JobShortPresentation.tsx
+++ src/components/JobShortPresentation.tsx
@@ -10,6 +10,7 @@ import {
 import { IJob } from '../models/IJob';
 import { Link } from 'react-router-dom';
 import { StyledJobShortContainer } from './StyledJobShortContainer';
+import { StyledSpan } from './StyledSpan';
 
 interface IJobShortPresentationProps {
   job: IJob;
@@ -40,23 +41,36 @@ export const JobShortPresentation = ({ job }: IJobShortPresentationProps) => {
               <h3>{job.headline}</h3>
             </Link>
             <div>
-              {job.duration.label && <h4>Varaktighet: {job.duration.label}</h4>}
+              {job.duration.label && (
+                <h4>
+                  Varaktighet: <StyledSpan>{job.duration.label}</StyledSpan>
+                </h4>
+              )}
               {job.working_hours_type.label && (
-                <h4>Omfattning: {job.working_hours_type.label}</h4>
+                <h4>
+                  Omfattning:{' '}
+                  <StyledSpan>{job.working_hours_type.label}</StyledSpan>
+                </h4>
               )}
             </div>
           </section>
           {job.workplace_address.municipality && (
-            <h4>Ort: {job.workplace_address.municipality}</h4>
+            <h4>
+              Ort: <StyledSpan>{job.workplace_address.municipality}</StyledSpan>
+            </h4>
           )}
-          <h4>Titel: {job.occupation.label}</h4>
           <h4>
-            Publicerad:&nbsp;
-            <DigiTypographyTime
-              afVariation={TypographyTimeVariation.PRETTY}
-              afDateTime={job.publication_date}
-              afLocale="sv-SE"
-            ></DigiTypographyTime>
+            Titel: <StyledSpan>{job.occupation.label}</StyledSpan>
+          </h4>
+          <h4>
+            Publicerad:{' '}
+            <StyledSpan>
+              <DigiTypographyTime
+                afVariation={TypographyTimeVariation.DISTANCE}
+                afDateTime={job.publication_date}
+                afLocale="sv-SE"
+              ></DigiTypographyTime>
+            </StyledSpan>
           </h4>
         </StyledJobShortContainer>
       </DigiTypography>
